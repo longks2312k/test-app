b6a1e222eccd4be867695ffb52bc12bea53d873a
longks2312k <longks2312k@gmail.com>
2022-01-13 10:29:50 +0700

    list-news

--- src/components/ListNew/index.jsx
+++ src/components/ListNew/index.jsx
@@ -1,9 +1,47 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
+import './style.scss'
+
+const newList = [
+    {
+        'images' : 'https://react-plus.com/images/pages/HomePage/1.jpg',
+        'title' : 'ReactとReact Nativeの テクノロジー企業',
+        'text' : '当社はReactとReact Nativeのスペシャリストです。',
+    },
+    {
+        'images' : 'https://react-plus.com/images/pages/HomePage/2.jpg',
+        'title' : '24時間365日対応の迅速なレスポンス',
+        'text' : 'Slackにご投稿いただければ、時間を問わず迅速にサポートいたします。',
+    },
+    {
+        'images' : 'https://react-plus.com/images/pages/HomePage/3.jpeg',
+        'title' : '真のエキスパート',
+        'text' : '当社の開発者は、人生の大半をReactとReact Nativeに情熱を注いでいると言っても過言ではありません。',
+    },
+]
 
 function ListNew(props) {
     return (
-        <div>
-            
+        <div className='list-new'>
+            <div className='list-title'>
+                <h1 className='title-1'>当社はReactとReact Nativeのスペシャリストです。</h1>
+                <div className='under-line'></div>
+                <h3 className='title-2'>ReactとReact Nativeを利用したプロジェクト開発サービスを提供しています。</h3>
+                <h3 className='title-2'>Reactエキスパート人材が豊富な当社が、お客様のご要望に的確にお応えいたします。</h3>
+            </div>
+            <div className="list-item">
+                {
+                    newList.map((data, i) => {
+                        return(
+                            <div key={i} className="item">
+                                <img className='new-img' src={data.images}/>
+                                <h4 className='title-news'>{data.title}</h4>
+                                <h4 className="text-news">{data.text}</h4>
+                            </div>
+                        )
+                    })
+                }
+            </div>
         </div>
     );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import Header from '../components/Header'
 import Banner from '../components/Banner'
 import Footer from '../components/Footer'
+import ListNew from '../components/ListNew'
 
 export default function Home() {
     return (
         <div>
-            <Header/>
-            <Banner/>
-            <Footer/>
+            <Header />
+            <Banner />
+            <ListNew />
+            <Footer />
         </div>
     )
 }
